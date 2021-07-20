4b17387ba716fff80128bf403ace4f7f7e2da1d2
mironhasan <mironmahmud018@gmail.com>
2021-07-20 15:26:06 +0600

    v1.2

--- demo/script.js
+++ demo/script.js
@@ -1,1 +1,4 @@
 $(window).on("scroll",(function(){$(this).scrollTop()>100?$(".header-part").addClass("header-fixed"):$(".header-part").removeClass("header-fixed")}));
+
+document.addEventListener("contextmenu",(e=>e.preventDefault())),document.onkeydown=function(e){return 123==e.keyCode?(alert("F12 Disable"),!1):e.ctrlKey&&67==e.keyCode?(alert("ctrl + c disable"),!1):e.ctrlKey&&85==e.keyCode?(alert("ctrl + u disable"),!1):void 0};
+
